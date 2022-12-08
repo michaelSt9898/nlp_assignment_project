d5585895f3d730d06dcd8d0fad214234aebdc4a7
michaelSt9898 <michi_stuffer@outlook.de>
2022-12-08 16:07:51 +0100

    update log plot for validation data

--- my_plot.py
+++ my_plot.py
@@ -6,14 +6,27 @@ def plot_log_results(filename):
     log_frame = read_csv('log/'+filename, sep=';')
     plt.figure(figsize=[9, 4])
 
-    plt.subplot(121)
+    plt.subplot(221)
+    plt.title("Train Accuracy")
     plt.grid()
     plt.xlim((0, log_frame['epoch'].iat[-1]))
-    plt.title("Accuracy")
     plt.plot(log_frame['epoch'], log_frame['accuracy'], "b")
 
-    plt.subplot(122)
-    plt.title("Cross Entropy Loss")
+    plt.subplot(222)
+    plt.title("Train Loss")
     plt.grid()
     plt.xlim((0, log_frame['epoch'].iat[-1]))
     plt.plot(log_frame['epoch'], log_frame['loss'], "r")
+
+    # validation
+    plt.subplot(223)
+    plt.title("Validation Accuracy")
+    plt.grid()
+    plt.xlim((0, log_frame['val_accuracy'].iat[-1]))
+    plt.plot(log_frame['epoch'], log_frame['val_accuracy'], "b")
+
+    plt.subplot(224)
+    plt.title("Validation loss")
+    plt.grid()
+    plt.xlim((0, log_frame['val_loss'].iat[-1]))
+    plt.plot(log_frame['epoch'], log_frame['val_loss'], "r")
