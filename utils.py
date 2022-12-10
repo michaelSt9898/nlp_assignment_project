bde4021bb9071a0067758bb8d33996704f8453f8
schererant <67601986+schererant@users.noreply.github.com>
2022-12-10 13:40:54 +0100

    Update training

---
+++ utils.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
+def plot_history(history):
+    loss_list = [s for s in history.history.keys() if 'loss' in s and 'val' not in s]
+    val_loss_list = [s for s in history.history.keys() if 'loss' in s and 'val' in s]
+    acc_list = [s for s in history.history.keys() if 'acc' in s and 'val' not in s]
+    val_acc_list = [s for s in history.history.keys() if 'acc' in s and 'val' in s]
+
+    if len(loss_list) == 0:
+        print('Loss is missing in history')
+        return
+
+    ## As loss always exists
+    epochs = range(1,len(history.history[loss_list[0]]) + 1)
+
+    ## Loss
+    plt.figure(1)
+    for l in loss_list:
+        plt.plot(epochs, history.history[l], 'b', label='Training loss (' + str(str(format(history.history[l][-1],'.5f'))+')'))
+    for l in val_loss_list:
+        plt.plot(epochs, history.history[l], 'g', label='Validation loss (' + str(str(format(history.history[l][-1],'.5f'))+')'))
+
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    ## Accuracy
+    plt.figure(2)
+    for l in acc_list:
+        plt.plot(epochs, history.history[l], 'b', label='Training accuracy (' + str(format(history.history[l][-1],'.5f'))+')')
+    for l in val_acc_list:
+        plt.plot(epochs, history.history[l], 'g', label='Validation accuracy (' + str(format(history.history[l][-1],'.5f'))+')')
+
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+def full_multiclass_report(model,
+                           x,
+                           y_true,
+                           classes,
+                           batch_size=32,
+                           binary=False):
+
+    # 1. Transform one-hot encoded y_true into their class number
+    if not binary:
+        y_true = np.argmax(y_true, axis=1)
+
+    # 2. Predict classes and stores in y_pred
+    y_pred = model.predict(x) #batch?
+    if not binary:
+        y_pred = np.argmax(y_pred, axis=1)
+
+    # 3. Print accuracy score
+    print("Accuracy : "+ str(accuracy_score(y_true, y_pred)))
+
+    print("")
+    print(y_pred)
+    print(y_true)
+    # 4. Print classification report
+    print("Classification Report")
+    print(classification_report(y_true, y_pred, digits=5))
+
+    # 5. Plot confusion matrix
+    cnf_matrix = confusion_matrix(y_true, y_pred)
+    print(cnf_matrix)
+    # plot_confusion_matrix(cnf_matrix,classes=classes)
